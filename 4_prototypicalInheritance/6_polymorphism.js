d69b1613faa77f3ce1d820011be37f10396272b7
Lalo Aguilar <tallitosan@gmail.com>
2021-02-28 02:10:33 -0600

    6_polymorphism.js

---
+++ 4_prototypicalInheritance/6_polymorphism.js
@@ -0,0 +1,40 @@
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype)
+  Child.prototype.constructor = Child
+}
+
+function Shape() {}
+
+Shape.prototype.duplicate = function () {
+  console.log('duplicate')
+}
+
+function Circle() {}
+
+extend(Circle, Shape)
+
+Circle.prototype.duplicate = function () {
+  console.log('duplicate circle')
+}
+
+function Square() {}
+
+extend(Square, Shape)
+
+Square.prototype.duplicate = function () {
+  console.log('duplicate square')
+}
+
+const shapes = [
+  // each of items have their custom .duplicate method
+  new Circle(),
+  new Square(),
+]
+
+// this prints:
+// duplicate circle
+// duplicate square
+for (let shape of shapes) {
+  shape.duplicate()
+}
+
